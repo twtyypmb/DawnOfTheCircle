57b549c06a3b719f37180b739c253eeec973eed4
xuliang <xul@cy-coo.com>
2020-01-30 11:33:01 +0800

    修正代码位置

--- DawnOfTheCircle/CommonResource.cpp
+++ DawnOfTheCircle/CommonResource.cpp
@@ -16,165 +16,166 @@ PIMAGE total_back_surface;
 PIMAGE  single_total_surface[SINGLE_TOTAL_SURFACE_MAX_LENGTH];
 
 
-void FreeSDLSurface(PIMAGE p){if(p!=NULL)delimage(p);}
-void FreeSDLTexture(PIMAGE p){if(p!=NULL)delimage(p);}
+void FreeSDLSurface(PIMAGE p) { if (p != NULL)delimage(p); }
+void FreeSDLTexture(PIMAGE p) { if (p != NULL)delimage(p); }
 
 
-bool InitResource( void )
+bool InitResource(void)
 {
-    memset(single_total_surface,0,SINGLE_TOTAL_SURFACE_MAX_LENGTH);
+	memset(single_total_surface, 0, SINGLE_TOTAL_SURFACE_MAX_LENGTH);
 	initgraph(640, 480);
 	randomize();
 	setrendermode(RENDER_MANUAL);
+	SetWindowText(GetWindow(), TEXT("CASTALVANIA---DAWN OF THE CIRCLE"));
 	return true;
 }
 
-HWND GetWindow( void )
+HWND GetWindow(void)
 {
-    if( _window == NULL)
-    {
+	if (_window == NULL)
+	{
 		_window = getHWnd();
-    }
+	}
 
-    return _window;
+	return _window;
 }
 
-PIMAGE GetRenderer(void )
+PIMAGE GetRenderer(void)
 {
-    
 
-    return _renderer;
+
+	return _renderer;
 }
 
 
 
-// 加载图片为纹理
-PIMAGE LoadImage(const char* path)
-{
-	getimage(_total_image, path, 0, 0);
-    return _total_image;
-}
+
 
 
 // 初始化公共资源
 PIMAGE GetTotalSurface()
 {
-    if(NULL == total_surface)
-    {
-        total_surface = LoadImage("resource/image/total.bmp");
-
-    }
-    return total_surface;
+	if (NULL == total_surface)
+	{
+		total_surface = newimage();
+		getimage(total_surface, "resource/image/total.bmp", 0, 0);
+	}
+	return total_surface;
 }
 
 PIMAGE GetTotalBackSurface()
 {
-    if(NULL == total_back_surface)
-    {
-        total_back_surface = LoadImage("resource/image/total_back.bmp");
+	if (NULL == total_back_surface)
+	{
+		total_back_surface = newimage();
+		getimage(total_back_surface, "resource/image/total_back.bmp", 0, 0);
 
-    }
-    return total_back_surface;
+	}
+	return total_back_surface;
 }
 
 
 
-PIMAGE GetTotalImageBack()
-{
-    if(NULL==_total_image)
-    {
-       _total_image = LoadImage("resource/image/total_image_back.bmp");
-    }
-    return _total_image;
-}
 
 
 PIMAGE* GetShockWave()
 {
-    if(NULL==_shock_wave[0])
-    {
-       _shock_wave[0] = LoadImage("resource/image/shock_wave_0.jpg");
-       _shock_wave[1] = LoadImage("resource/image/shock_wave_1.jpg");
-       _shock_wave[2] = LoadImage("resource/image/shock_wave_2.jpg");
-       _shock_wave[3] = LoadImage("resource/image/shock_wave_3.jpg");
-    }
-    return _shock_wave;
+	/*if(NULL==_shock_wave[0])
+	{
+	   _shock_wave[0] = LoadImage("resource/image/shock_wave_0.jpg");
+	   _shock_wave[1] = LoadImage("resource/image/shock_wave_1.jpg");
+	   _shock_wave[2] = LoadImage("resource/image/shock_wave_2.jpg");
+	   _shock_wave[3] = LoadImage("resource/image/shock_wave_3.jpg");
+	}*/
+	return _shock_wave;
 }
 
 PIMAGE GetDescription()
 {
-    if(NULL==_description)
-    {
-       _description = LoadImage("resource/image/description.jpg");
-    }
-    return _description;
+	if (NULL == _description)
+	{
+		//_description = LoadImage("resource/image/description.jpg");
+	}
+	return _description;
 }
 
 PIMAGE GetDoors()
 {
-    if(NULL==_doors)
-    {
-       _doors = LoadImage("resource/image/doors.jpg");
-    }
-    return _doors;
+	if (NULL == _doors)
+	{
+		//_doors = LoadImage("resource/image/doors.jpg");
+	}
+	return _doors;
 }
 
 PIMAGE GetGameLogo()
 {
-    if(NULL==_game_log)
-    {
-       _game_log = LoadImage("resource/image/game_logo.jpg");
-    }
-    return _game_log;
+	if (NULL == _game_log)
+	{
+		//_game_log = LoadImage("resource/image/game_logo.jpg");
+	}
+	return _game_log;
 }
 
 PIMAGE GetTheEnd()
 {
-    if(NULL==_the_end)
-    {
-       _the_end = LoadImage("resource/image/the_end.jpg");
-    }
-    return _the_end;
+	if (NULL == _the_end)
+	{
+		//_the_end = LoadImage("resource/image/the_end.jpg");
+	}
+	return _the_end;
 }
 
 
 PIMAGE GetGameOver()
 {
-    if(NULL==_game_over)
-    {
-       _game_over = LoadImage("resource/image/game_over.jpg");
-    }
-    return _game_over;
+	if (NULL == _game_over)
+	{
+		//_game_over = LoadImage("resource/image/game_over.jpg");
+	}
+	return _game_over;
 }
 
 
 // 释放公共资源
-void FreeAllCommonResource( void )
+void FreeAllCommonResource(void)
 {
-    delimage(_total_image);
-    //SDL_free(_total_image_back);
+	delimage(_total_image);
+	//SDL_free(_total_image_back);
 
-    if(NULL != _renderer)
-    {
+	if (NULL != _renderer)
+	{
 		delimage(_renderer);
-        _renderer= NULL;
-    }
+		_renderer = NULL;
+	}
 
-    if(NULL != _window)
-    {
+	if (NULL != _window)
+	{
 		/*delimage(_window);
-        _window= NULL;*/
-    }
+		_window= NULL;*/
+	}
+}
+PIMAGE GetSingleImage(int number)
+{
+	PIMAGE result = single_total_surface[number];
+	if (result == NULL)
+	{
+		result = newimage(OBJECT_WIDTH, OBJECT_HEIGHT);
+		PIMAGE total_img1, total_img2, img1, img2;
+		total_img1 = newimage();
+		total_img2 = newimage();
+		img1 = newimage();
+		img2 = newimage();
+
+		getimage(total_img1, "resource/image/total_back.bmp", 0, 0);
+		getimage(total_img2, "resource/image/total_back.bmp", 0, 0);
+		getimage(img1, total_img1, number % 11 * 32, number / 11 * 32, 32, 32);
+		getimage(img2, total_img2, (number % 11) * 32, number / 11 * 32, 32, 32);
+		putimage_transparent(img2, img1, 0, 0, 0x000000, 0, 0, 32, 32);//图像重合
+
+		getimage(img2, total_img2, 1 * 32, 17 * 32, 32, 32);
+	}
+
+	return result;
 }
 
-
-//SDL_Event* GetSDLEvnet( void )
-//{
-//    return &event;
-//}
-//
-//SDL_Event* PollSDLEvent( void )
-//{
-//    SDL_PollEvent(&event);
-//    return &event;
-//}
--- DawnOfTheCircle/CommonResource.h
+++ DawnOfTheCircle/CommonResource.h
@@ -11,34 +11,33 @@
 #define OBJECT_HEIGHT                               32
 
 
-static HWND _window=NULL;
-static PIMAGE _renderer=NULL;
+static HWND _window = NULL;
+static PIMAGE _renderer = NULL;
 //static SDL_Event event;
 static PIMAGE _total_image = NULL;
-static PIMAGE _total_image_back= NULL;
+static PIMAGE _total_image_back = NULL;
 static PIMAGE _shock_wave[4];
-static PIMAGE _doors= NULL;
-static PIMAGE _game_log= NULL;
-static PIMAGE _game_over= NULL;
-static PIMAGE _the_end= NULL;
-static PIMAGE _description= NULL;
+static PIMAGE _doors = NULL;
+static PIMAGE _game_log = NULL;
+static PIMAGE _game_over = NULL;
+static PIMAGE _the_end = NULL;
+static PIMAGE _description = NULL;
 
 /*******************/
-bool InitResource( void );
-PIMAGE LoadImage(const char* path);
-HWND GetWindow(void );
-PIMAGE GetRenderer(void );
+bool InitResource(void);
+HWND GetWindow(void);
+PIMAGE GetRenderer(void);
 PIMAGE GetTotalImage(void);
-PIMAGE GetTransparentTexture(PIMAGE surface, int number, PIMAGE surface_back, int number_back );
+PIMAGE GetTransparentTexture(PIMAGE surface, int number, PIMAGE surface_back, int number_back);
 PIMAGE* GetShockWave(void);
 PIMAGE GetDescription(void);
 PIMAGE GetDoors(void);
 PIMAGE GetGameLogo(void);
 PIMAGE GetTheEnd(void);
 PIMAGE GetGameOver(void);
-void FreeAllCommonResource( void );
-PIMAGE GetTotalBackSurface(void);
+void FreeAllCommonResource(void);
 PIMAGE GetTotalSurface(void);
+PIMAGE GetTotalBackSurface(void);
 //SDL_Event* GetSDLEvnet( void );
 //SDL_Event* PollSDLEvent( void );
 
